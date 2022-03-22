24006095facc73a3fcb19542cf885e08bdf8a318
Richard Shade <rshade98@hotmail.com>
2022-03-22 06:54:45 -0500

    Updates

--- pulumi-quest-4/__main__.py
+++ pulumi-quest-4/__main__.py
@@ -1,21 +1,17 @@
 """A Kubernetes Python Pulumi program"""
 
 import pulumi
-from pulumi_kubernetes.apps.v1 import Deployment, DeploymentSpecArgs
-from pulumi_kubernetes.meta.v1 import LabelSelectorArgs, ObjectMetaArgs
-from pulumi_kubernetes.core.v1 import ContainerArgs, PodSpecArgs, PodTemplateSpecArgs
+from pulumi_kubernetes.yaml import ConfigGroup
+from pulumi_kubernetes.yaml import ConfigFile
+from pulumi import export
 
-app_labels = { "app": "nginx" }
+civo_quest_4_deployment = ConfigFile("quest4_deployment", "../quest-4/deployment.yaml")
+deployment = civo_quest_4_deployment.get_resource("apps/v1/Deployment", "hello-world")
+civo_quest4_service = ConfigFile("quest4_service", "../quest-4/service.yaml")
+frontend = civo_quest4_service.get_resource("v1/Service", "hello-world")
+civo_quest_4_ingrest = ConfigFile("quest4_ingress", "../quest-4/ingress.yaml")
+export(
+  name="privateIp",
+  value=frontend.spec.cluster_ip)
 
-deployment = Deployment(
-    "nginx",
-    spec=DeploymentSpecArgs(
-        selector=LabelSelectorArgs(match_labels=app_labels),
-        replicas=1,
-        template=PodTemplateSpecArgs(
-            metadata=ObjectMetaArgs(labels=app_labels),
-            spec=PodSpecArgs(containers=[ContainerArgs(name="nginx", image="nginx")])
-        ),
-    ))
-
-pulumi.export("name", deployment.metadata["name"])
+export("name", deployment.metadata["name"])
